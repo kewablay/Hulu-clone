f161b3ec57e5c9fc6b31c840241c529e3093c131
kewablay <kojokewajunior123@gmail.com>
2022-07-28 20:58:52 +0000

    added smooth animation and tweaks to the UI

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,17 +1,16 @@
 import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
 
 function MovieList({ results }) {
   return (
-    <div
+    <FlipMove
       className="px-5 py-5 sm:grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 3xl-flex justify-center flex-wrap
     "
     >
       {results.map((result) => (
-        <div>
-          <Thumbnail key={result.id} result={result} />
-        </div>
+        <Thumbnail key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 }
 
